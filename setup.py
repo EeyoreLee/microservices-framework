96a4145f05afca45a081de92cecf4db385c85b47
earlee <li.chunyu0412@outlook.com>
2021-12-10 16:33:51 +0800

    setup script update version 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setuptools.setup(
     name="msf",
-    version="0.1.0",
+    version="0.1.1",
     author="EeyoreLee",
     author_email="li.chunyu0412@outlook.com",
     description="python easy microservices framework",
