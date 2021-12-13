749cc4f83f92af5e0ce4ea70d382a17e2f69651d
earlee <li.chunyu0412@outlook.com>
2021-12-13 11:42:26 +0800

    feature(engine): Engine add global storage var

--- example/nodes/test.py
+++ example/nodes/test.py
@@ -10,8 +10,11 @@ from msf import node_register
 
 @node_register()
 def test_node_via_print(**kwds):
+    resource = kwds.get('resource', {})
+    resource_apple = resource.get('test_rsc')
     param = kwds.get('ids', [])
     result = {
         'result': param
     }
+    print(resource_apple.apple)
     return result
--- example/run.py
+++ example/run.py
@@ -9,11 +9,19 @@ from example.workflow_conf import NODE_CONF, PATH_CONF
 from example.nodes import *
 
 
+class ResourceTest(object):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.apple = 'apppppp'
+
 def main():
 
     config = {
         'NODE_CONF': NODE_CONF,
         'PATH_CONF': PATH_CONF
     }
-    engine = Engine(config)
+    resource_apple = ResourceTest()
+    resource = {'test_rsc': resource_apple}
+    engine = Engine(config, resource=resource)
     engine.run(host='0.0.0.0', port='41000')
