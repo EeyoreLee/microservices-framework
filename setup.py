65ca0ff393524414d5e728074c03ae44b58a5dbf
earlee <li.chunyu0412@outlook.com>
2021-12-13 14:50:53 +0800

    bugfix(setup): fix setup `include` of packages find_packages

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setuptools.setup(
     name="msf",
-    version="0.1.2",
+    version="0.1.3",
     author="EeyoreLee",
     author_email="li.chunyu0412@outlook.com",
     description="python easy microservices framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/EeyoreLee/microservices-framework",
-    packages=setuptools.find_packages(include=['msf']),
+    packages=setuptools.find_packages(include=['msf', 'msf.*']),
     install_requires=['flask>=2.0.2'],
     classifiers=(
         "Programming Language :: Python :: 3",
@@ -26,6 +26,6 @@ setuptools.setup(
     ),
     python_requires='>=3.6',
     package_data={
-        'msf': ['']
+        'msf': ['*']
     }
 )
