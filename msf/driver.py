9ae2d368bd76c0b0962dc18bab3c28c104128d74
EeyoreLee <li.chunyu0412@outlook.com>
2022-07-19 18:27:25 +0800

    provisional commit

--- msf/driver.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-'''
-@create_time: 2022/02/09 13:43:06
-@author: lichunyu
-'''
-
-import logging
-
-logger = logging.getLogger(__name__)
---
+++ msf/utils/logging_utils.py
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+'''
+@create_time: 2022/07/19 14:11:41
+@author: lichunyu
+'''
+from logging.config import dictConfig
+
+
+_logconfig_dict_default = {
+    "version": 1,
+    "incremental": False,
+    "disable_existing_loggers": False,
+    "root": {
+        "level": "INFO",
+        "handlers": ["console"]
+    },
+    "formatters": {
+        "default": {
+            "class": "logging.Formatter",
+            "format": "%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S %z"
+        }
+    },
+    "handlers": {
+        "timed_rotating_file_handler": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "formatter": "default",
+            "filename": "log.log",
+            "when": "D",
+            "backupCount": 7,
+            "interval": 1,
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "stream": "ext://sys.stdout"
+        },
+    },
+    "loggers": {
+        "msf": {
+            "level": "INFO",
+            "handlers": ["timed_rotating_file_handler", "console"],
+            "propagate": False,
+            "qualname": "msf"
+        }
+    }
+}
+
+
+def logger_setup(logconfig_dict):
+    config = _logconfig_dict_default.copy()
+    config.update(logconfig_dict)
+    try:
+        dictConfig(config)
+    except (
+            AttributeError,
+            ImportError,
+            ValueError,
+            TypeError
+    ) as exc:
+        raise RuntimeError(str(exc))
